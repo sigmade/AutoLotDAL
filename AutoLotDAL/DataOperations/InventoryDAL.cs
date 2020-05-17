2a046819d43d449d9a51ad7d7ae8fbd6ce7f3cdd
sigmade <sychyov1991@mail.ru>
2020-05-17 21:47:26 +0300

    Add CRUD

--- AutoLotDAL/DataOperations/InventoryDAL.cs
+++ AutoLotDAL/DataOperations/InventoryDAL.cs
@@ -57,6 +57,94 @@ namespace AutoLotDAL.DataOperations
             return inventory;
         }
 
+        public Car GetCar(int id) // return one row for CarId
+        {
+            OpenConnection();
+            Car car = null;
+            string sql = $"SELECT * FROM Inventory WHERE CarId = {id}";
+            using (SqlCommand command = new SqlCommand(sql, _sqlConnection))
+            {
+                command.CommandType = CommandType.Text;
+                SqlDataReader dataReader = command.ExecuteReader(CommandBehavior.CloseConnection);
+                while (dataReader.Read())
+                {
+                    car = new Car
+                    {
+                        CarId = (int)dataReader["CarId"],
+                        Color = (string)dataReader["Color"],
+                        Make = (string)dataReader["Make"],
+                        PetName = (string)dataReader["Petname"]
+                    };                    
+                }
+                dataReader.Close();
+            }
+            return car;
+        }
+        public void InsertAuto(string color, string make, string petName)
+        {
+            OpenConnection();
+            // Format and execute SQL statement.
+            string sql = $"Insert Into Inventory (Make, Color, PetName) Values ('{make}', '{color}', '{petName}')";
+
+            // Execute using our connection.
+            using (SqlCommand command = new SqlCommand(sql, _sqlConnection))
+            {
+                command.CommandType = CommandType.Text;
+                command.ExecuteNonQuery();
+            }
+            CloseConnection();
+        }
+        //public void InsertAuto(Car car) // type method
+        //{
+        //    OpenConnection();
+        //    string sql = "INSERT INTO Inventory (Make, Color, Petname) VALUES ('{car.Make}', '{car.Color}', '{car.PetNmae}')";
+
+        //    using (SqlCommand command = new SqlCommand(sql, _sqlConnection))
+        //    {
+        //        command.CommandType = CommandType.Text;
+        //        command.ExecuteNonQuery();
+        //    }
+        //    CloseConnection();
+        //}
+
+        public void InsertAuto(Car car)
+        {
+            OpenConnection();
+            string sql = "INSERT INTO Inventory (Make, Color, PetName) VALUES (@Make, @Color, @PetName)";
+
+        }
+        public void DeleteCar(int id)
+        {
+            OpenConnection();
+            string sql = $"DELETE FROM Inventory WHERE CarId = '{id}'";
+            using (SqlCommand command = new SqlCommand(sql, _sqlConnection))
+            {
+                try
+                {
+                    command.CommandType = CommandType.Text;
+                    command.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    Exception error = new Exception("Sorry! That car is on order!", ex);
+
+                    throw error;
+                }
+            }
+            CloseConnection();
+        }
+        public void UpdateCarPetName(int id, string newPetName)
+        {
+            OpenConnection();
+            string sql = $"UPDATE Inventory SET PetName = '{newPetName}' WHERE CarId = '{id}'";
+            using (SqlCommand command = new SqlCommand(sql, _sqlConnection))
+            {
+                command.ExecuteNonQuery();
+            }
+            CloseConnection();
+        }
+
+
 
     }
 }
